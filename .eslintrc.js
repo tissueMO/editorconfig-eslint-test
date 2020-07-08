8f078f16069d5300399c5a6118552ef2308e2167
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2020-07-09 01:28:36 +0900

    ESLint 設定修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,28 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2020': true
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
   },
-  'plugins': [
-    'prettier',
-  ],
-  'extends': [
-    'eslint:recommended',
-    'prettier/recommended'
-  ],
-  'parserOptions': {
-    'ecmaVersion': 11
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 11,
   },
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'windows'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
     'prettier/prettier': [
       'error',
       {
-        'singleQuote': true,
-        'semi': true
-      }
+        singleQuote: true,
+        semi: true,
+      },
     ],
     'no-var': 'error',
     'prefer-const': 'error',
     'object-shorthand': 'error',
-    'prefer-arrow-callback': 'error'
-  }
+    'prefer-arrow-callback': 'error',
+  },
 };
